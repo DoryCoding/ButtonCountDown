4119bca8586c6d457572491245afbbc409815fbe
Dory <zoudaoli@163.com>
2016-06-23 20:53:18 +0800

    Initial Commit

---
+++ ButtonCountDownDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonCountDownDemo
+//
+//  Created by Dory on 23/6/16.
+//  Copyright © 2016年 www.visualogies.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonCountDownDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonCountDownDemo
+//
+//  Created by Dory on 23/6/16.
+//  Copyright © 2016年 www.visualogies.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
